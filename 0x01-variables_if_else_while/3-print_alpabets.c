bafcef0d2a5e118553427199fb32e176d286326e
Ytemiloluwa <ytemiloluwa@gmail.com>
2022-09-09 23:04:08 +0100

    Hot fix

--- 0x01-variables_if_else_while/3-print_alpabets.c
+++ 0x01-variables_if_else_while/3-print_alpabets.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase.
+ * main - Prints the alphabet in lowercase, and then in uppercase.
  *
  * Return: Always 0.
- *
- * Task 2 
  */
 int main(void)
 {
-     char lowercase;
+	char lowercase;
+
      char UpperCase;
 
-     for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 		putchar(lowercase);
 
      for (UpperCase = 'A'; UpperCase <= 'Z'; UpperCase++)
@@ -21,3 +20,4 @@ int main(void)
 
 	return (0);
 }
+
