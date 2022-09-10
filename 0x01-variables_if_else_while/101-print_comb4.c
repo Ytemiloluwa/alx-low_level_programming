b89c6055b0a44569df44b69ec9d1f027aa2af890
Ytemiloluwa <ytemiloluwa@gmail.com>
2022-09-10 01:40:22 +0100

    program that prints all possible different combinations of three digits.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: display triple digits and ,
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	i = 0;
+	while (i < 1000)
+	{
+		j = i / 100; /* hundreds */
+		k = (i / 10) % 10; /* tens */
+		l = i % 100; /* units */
+		if (j < k && k < l)
+		{
+			putchar(l + '0');
+			putchar(k + '0');
+			putchar(j + '0');
+			if (i < 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
