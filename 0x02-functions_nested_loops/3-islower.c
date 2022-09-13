a58c20a042dd1c9fad1854bec5b33db1439c3e82
Ytemiloluwa <ytemiloluwa@gmail.com>
2022-09-13 23:23:45 +0100

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - Checks if a character is lowercase.
  *
- * Description: checks for lowercase character
+ * Description: @c The character to be checked.
  *
  * Return: 1 if character is lowercase, 0 otherwise.
  */
