d54369cd1ebb404d87e51ae474446df42dad1d2e
Ytemiloluwa <ytemiloluwa@gmail.com>
2022-09-19 21:31:46 +0100

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcpy - return a pointer copy of a string
+ * @dest: first para
+ * @src: second para
+ * Return: void
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
