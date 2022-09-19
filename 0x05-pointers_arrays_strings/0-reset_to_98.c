489d0380181f37345a5cf7a37fed6fe5a634b17d
Ytemiloluwa <ytemiloluwa@gmail.com>
2022-09-19 11:32:38 -0700

    main file and Task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- *  * reset_to_98 - changes a parameter's value to 98
- *   * @n: - the parameter to be changed
- *    */
+ * reset_to_98 - changes a parameter's value to 98
+ * @n: - the parameter to be changed
+ *
+ */
 
 void reset_to_98(int *n)
 {
