2a2b619df6c708809fb22ccde09620cd9f644951
Ytemiloluwa <ytemiloluwa@gmail.com>
2022-09-19 20:28:48 +0100

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints evey character at step 2
+ * @str: the character
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
